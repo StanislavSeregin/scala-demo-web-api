6eb99a222d50c0fdb7168b94d2d41a9e70386964
Stanislav Seregin <stas_seregin@mail.ru>
2020-04-12 19:02:41 +0300

    Add api endpoint

--- src/main/scala/com/srs/demowebapi/Main.scala
+++ src/main/scala/com/srs/demowebapi/Main.scala
@@ -12,11 +12,15 @@ import sttp.tapir.server.http4s._
 import scala.concurrent.ExecutionContext
 
 object Main extends IOApp {
-  val _endpoint: Endpoint[String, Unit, String, Nothing] =
-    endpoint.get.in("hello").in(query[String]("name")).out(stringBody)
-
   val routes: HttpRoutes[IO] =
-    _endpoint.toRoutes(name => IO(s"Hello, $name!".asRight[Unit]))
+    endpoint.get.in("hello").in(query[String]("name")).out(stringBody).toRoutes(successAction) <+>
+      endpoint.get.in("hello2").out(stringBody).toRoutes(failedAction)
+
+  def successAction(input: String): IO[Either[Unit, String]] =
+    IO(s"Hello, $input!".asRight)
+
+  def failedAction(unit: Unit): IO[Either[Unit, String]] =
+    IO(Either.left(()))
 
   def run(args: List[String]): IO[ExitCode] =
     WebApiApp.serverStream[IO](routes).compile.drain.as(ExitCode.Success)
